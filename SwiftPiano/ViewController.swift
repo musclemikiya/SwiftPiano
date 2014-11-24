b1720cdacf755266f692aac7c3edb1cf821fef46
MikiyaFukuda <mikiyafukuda@MikiyaFukuda-no-MacBook-Air.local>
2014-11-24 22:25:24 +0900

    Initial Commit

---
+++ SwiftPiano/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPiano
+//
+//  Created by MikiyaFukuda on 2014/11/24.
+//  Copyright (c) 2014年 MikiyaFukuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
