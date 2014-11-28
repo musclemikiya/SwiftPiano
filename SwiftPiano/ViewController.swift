727a79ff8fe3811ebf920fc40c70a48beec7cfa5
MikiyaFukuda <mikiyafukuda@MikiyaFukuda-no-MacBook-Air.local>
2014-11-28 12:35:58 +0900

    initial commit

--- SwiftPiano/ViewController.swift
+++ SwiftPiano/ViewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
+    let sounds = ["C3", "C#", "D", "D#","E","F", "F#", "G", "G#", "A", "A#", "B", "C4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func touchC(sender: UIButton) {
+        println(sender.tag)
+        let contentURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(sounds[sender.tag], ofType: "mp3")!)
+        audioPlayer = AVAudioPlayer(contentsOfURL: contentURL, error: nil)
+        audioPlayer.play()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
